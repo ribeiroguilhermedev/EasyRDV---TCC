0a1e357a1b2d113f20cc8a95fbab189e8c248fd5
Felipe Vidal <felipevgevaerd@gmail.com>
2023-03-30 22:34:29 -0300

    Adicionando rotas e organizando.

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import Login from '../pages/login'
+import Home from '../pages/home'
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+        <Route>
+          <Route index element={<Login/>} />
+          <Route path='/teste' element={'Teste'} />
+          {/* <Route path="/home" element={<Private Item={Home} />} /> */}
+          <Route path='*' element={<Login/>} />
+        </Route>
+))
+
+export default router
