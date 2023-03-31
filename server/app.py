6612286b52f19ceaada3bc25c19a00f1d418c405
William Bernardes Ferreira <william.ferreira@compasso.com.br>
2023-03-31 01:46:26 -0300

    adicionado configuração inicial de conexão com o banco de dados em sqlserver

--- server/app.py
+++ server/app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, Response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import pymssql
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://easyrdv:zPKd;9z;m$ftop9S@35.198.11.196/easyrdv'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+
+from model.user import User
 
 @app.route('/')
 def login():
     return render_template("index.html")
 
+
 if __name__ == "main":
-    app.run(host='0.0.0.0',debug=False) 
+    db.create_all()
+    app.run(host='0.0.0.0',debug=False)
     
 # Host é o comando para colocar o ip da máquina.
---
+++ server/model/user.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
