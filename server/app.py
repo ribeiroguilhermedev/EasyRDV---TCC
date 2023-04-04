8ae440ba976ad1b0c24a7548238403c1959c379a
William Bernardes Ferreira <william.ferreira@compasso.com.br>
2023-04-04 01:10:40 -0300

    add conexão com o banco de dados

--- server/app.py
+++ server/app.py
@@ -1,26 +1,23 @@
 from flask import Flask, render_template, Response
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import pymssql
+from db import cnxn   
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pymssql://easyrdv:zPKd;9z;m$ftop9S@35.198.11.196/easyrdv'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
-migrate = Migrate(app, db)
-
-from model.user import User
+# from model.user import User
 
 @app.route('/')
 def login():
-    return render_template("index.html")
+    return "Hello World"
 
 
 if __name__ == "main":
-    db.create_all()
-    app.run(host='0.0.0.0',debug=False)
+    app.run(debug=False)
+
+    
     
 # Host é o comando para colocar o ip da máquina.
