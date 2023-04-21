86566e658a989bf022e59e0d3eaec176b32bc4dd
Felipe Vidal <felipevgevaerd@gmail.com>
2023-04-21 20:21:39 -0300

    Adicionando rota de post para auth.

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -3,40 +3,62 @@ import businessTravel from '../../assets/business_travel.avif'
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../../auth/authContext";
 import { AuthenticatedUser } from "../../types/types";
-import {useQuery} from 'react-query'
-import axios from "axios";
 import apiClient from "../../services/api";
+import {useMutation} from 'react-query'
 
 const Login = () => {
     const { login } = useAuth();
     const navigate = useNavigate();
 
     const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState('');
+    const [password, setPassword] = useState<string>('');
+    // const [id, setId] = useState<number | null>(null);
+    // const [name_user, setNameUser] = useState<string | null>(null);
+    // const [token, setToken] = useState<string | null>(null);
 
   
-    const {data, isFetching} = useQuery('id', async() => {
-        const response = await apiClient.get('1')
+    // const {data, isFetching} = useQuery('id', async() => {
+    //     const response = await apiClient.get('empresa/1')
         
-        return response.data;
-    })
+    //     return response.data;
+    // })
+
+    const mutation = useMutation((auth: {email: String, senha:String}) => {
+        return apiClient.post('/auth', auth)
+        .then(response => {
+            console.log(response.data?.usuario?.id)
+            console.log(response.data?.usuario?.nome)
+            console.log(response.data?.token)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+      })
     
-   
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-       console.log(data)
+      
+      const handleSubmit = async (e: any) => {
+          e.preventDefault();
+          
+          mutation.mutate({ email:email, senha: password })
+      
+
+
         try {
             const added_time = 5 * 1000
             // const { user, token } = await signIn(email, password);
+
+
+
+
             var userTeste: AuthenticatedUser = {
-                email,
+                email: "bil@email.com",
                 id: 1,
-                name: "nome",
-                token: 'sdfhsihfsdhfo87ry8yt8e7tiye78ergoi',
+                name: 'teste',
+                token: 'eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUEkiLCJzdWIiOiIyIiwiaWF0IjoxNjgyMTE1OTcwLCJleHAiOjE2ODIyMDIzNzB9.8B0U5spBdEfICQhp0i7UFmn0J514-25HmAe3v9_smqk',
                 expires: Date.now() + added_time
             }
 
-            
+            console.log(userTeste)
 
             login(userTeste);
             navigate('/home');
@@ -54,13 +76,13 @@ const Login = () => {
             <div className="bg-blue-post flex flex-col justify-center">
                 <form className="max-w-[400px] w-full mx-auto bg-gray-700 p-8 px-8 rounded-lg" onSubmit={handleSubmit}>
                     <h2 className="text-4xl text-white font-bold text-center">LOGIN</h2>
-                    <div className="flex flex-col text-gray-300 py-2">
+                    <div id="email" className="flex flex-col text-gray-300 py-2">
                         <label htmlFor="">E-mail</label>
                         <input onChange={e => setEmail(e.target.value)} className="rounded-lg bg-gray-500 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none" type="text" />
                     </div>
-                    <div className="flex flex-col text-gray-300 py-2">
+                    <div id="password" className="flex flex-col text-gray-300 py-2">
                         <label htmlFor="">Senha</label>
-                        <input className="rounded-lg bg-gray-500 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none" type="password" />
+                        <input onChange={e => setPassword(e.target.value)}className="rounded-lg bg-gray-500 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none" type="password" />
                     </div>
                     <div className="flex justify-between text-gray-300 py-2">
                         <p className="flex items-center"><input type="checkbox" className="mr-2" /> Lembrar</p>
