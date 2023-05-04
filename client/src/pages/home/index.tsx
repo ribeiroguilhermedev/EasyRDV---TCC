1da3eb33da304d625cb3a55226b220a06a585252
Guilherme.Ribeiro <guilherme.ribeiro.01tec@gmail.com>
2023-05-04 20:18:14 -0300

    ajustes nos campos do form

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -21,176 +21,167 @@ import CardTravelIcon from '@mui/icons-material/CardTravel';
 const Home = () => {
   const auth = useAuth()
   const location = useLocation();
-  
+
   const [isOpen, setOpen] = useState(false);
-  
+
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
-  
+
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
-  
+
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
     setOpen(true)
   };
-  
+
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  
+
   const logout = () => {
     setAnchorElUser(null);
     auth.logout()
   };
-  
-  const pages = [{nome: 'Cadastrar', fClick: handleCloseNavMenu}];
-  const settings = [{nome: 'Perfil',fClick: handleCloseUserMenu}, {nome: 'Logout', fClick: logout}]
-  
+
+  function stringAvatar(name: string) {
+    return {
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };
+  }
+
+  const pages = [{ nome: 'Cadastrar', fClick: handleCloseNavMenu }];
+  const settings = [{ nome: 'Perfil', fClick: handleCloseUserMenu }, { nome: 'Logout', fClick: logout }]
+
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <CardTravelIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            EasyRDV
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+    <>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <CardTravelIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
               sx={{
-                display: { xs: 'block', md: 'none' },
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page.nome} onClick={page.fClick}>
-                  <Typography textAlign="center">{page.nome}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <CardTravelIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            EasyRDV
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page.nome}
-                onClick={page.fClick}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+              EasyRDV
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
               >
-                {page.nome}
-              </Button>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Abrir configurações">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt={location.state.currentUser?.name} src="/static/images/avatar/2.jpg" />
+                <MenuIcon />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page.nome} onClick={page.fClick}>
+                    <Typography textAlign="center">{page.nome}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <CardTravelIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting.nome} onClick={setting.fClick}>
-                  <Typography textAlign="center">{setting.nome}</Typography>
-                </MenuItem>
+              EasyRDV
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.nome}
+                  onClick={page.fClick}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page.nome}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-      <EmployeeRegister open={isOpen} onClose={() => setOpen(false)} />
-    </AppBar>
-    // <>
-    //   <header>
-    //     <AppBar>
-    //       <Toolbar className="flex justify-between items-center">
-    //         <Chip
-    //           avatar={<Avatar alt={location.state.currentUser?.name} src="/broken-image.jpg" />}
-    //           label={location.state.currentUser?.name}
-    //           variant="outlined"
-    //           size="medium"
-    //         />
-    //         <Button color="inherit" onClick={() => setOpen(true)} >Cadastrar</Button>
-    //         <Button color="inherit" onClick={() => auth.logout()} >Logout</Button>
-    //       </Toolbar>
-    //     </AppBar>
-    //   </header>
-    // </>
+            </Box>
 
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Abrir configurações">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar {...stringAvatar(`${location.state.currentUser?.name} ${location.state.currentUser?.sobrenome}`)} src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting.nome} onClick={setting.fClick}>
+                    <Typography textAlign="center">{setting.nome}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <EmployeeRegister open={isOpen} onClose={() => setOpen(false)} />
+    </>
   )
 }
 
