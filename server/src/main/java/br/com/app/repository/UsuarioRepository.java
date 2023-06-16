36fb572cccbc1cf105fc3a2948a47fd1c57cc844
Guilherme.Ribeiro <guilherme.ribeiro.01tec@gmail.com>
2023-06-16 12:52:02 -0300

    commit do filtro de func

--- server/src/main/java/br/com/app/repository/UsuarioRepository.java
+++ server/src/main/java/br/com/app/repository/UsuarioRepository.java
@@ -5,6 +5,8 @@ import br.com.app.modelo.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +15,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	Optional<Usuario> findByEmailOrCpfOrRg(String email, String cpf, String rg);
 	Optional<Usuario> findByGuid(String guid);
 	List<Usuario> findByNome(String nome);
+
+	@Query(value = "SELECT u.* FROM usuario u JOIN usuario_perfis up ON u.id = up.usuario_id WHERE u.empresa_id = ?1 AND up.perfis_id = 2", nativeQuery = true)
 	Page<Usuario> findAllByEmpresa_id(Long empresa_id, Pageable pageable);
 }
